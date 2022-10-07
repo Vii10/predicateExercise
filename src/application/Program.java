bd9866e09042687a139614bef3dee0924bc977c6
The John! <jdworks1998@gmail.com>
2022-10-06 23:58:38 -0300

    Implemented Predicate Class

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import util.PredicateImp;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		List<Product> p = new ArrayList<>();
+		
+		p.add(new Product("Tv", 900.00));
+		p.add(new Product("Mouse", 50.00));
+		p.add(new Product("Tablet", 350.50));
+		p.add(new Product("HD Case", 80.90));
+		
+		p.removeIf(new PredicateImp());
+		
+		for(Product product : p) {
+			System.out.println(product);
+		}
+		
+	}
+
+}
